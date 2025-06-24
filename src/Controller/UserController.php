5c7a05959c55a187bb35f43bf0edb00ef5a8b490
GaetanThomas42 <gaetanthomasdev@gmail.com>
2025-06-24 10:30:06 +0200

    401 token user not found

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,6 +20,6 @@ final class UserController extends AbstractController
     {
         /** @var User $user */
         $user = $this->getUser();
-        return new JsonResponse(!$user ? ['error' => 'User not found'] : $this->normalizer->normalize($user, null, ["groups" => ["user:read"]]) );
+        return new JsonResponse(!$user ? ['error' => 'User not found'] : $this->normalizer->normalize($user, null, ["groups" => ["user:read"]]), !$user ? 401:200);
     }
 }
